9153865c35b4dabac1b1e62806aa7ed7b5ee3307
lizhixu <31006663+wxuns@users.noreply.github.com>
2019-02-28 08:27:30 +0800

    Delete routes.php

--- conf/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Route::get('/', 'IndexController@index');
-Route::get('/login', 'IndexController@loginShow');
-Route::post('/login', 'IndexController@login');
-
-Route::group(['prefix' => '/admin', 'middleware' => 'admin'],function () {
-    Route::get('/', 'HomeController@index');
-});
